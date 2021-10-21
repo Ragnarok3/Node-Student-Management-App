98d77e6df2a75be708dcda4456f94b9809e02acd
Amit Saxena <saxenaamit264@gmail.com>
2021-10-22 03:21:57 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const path = require('path')
+const handlebars = require('handlebars')
+const hbs = require('hbs')
+
+
+
+const app = express()
+
+//Define paths for express config
+const publicDirPath = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//setup handebars engine and views location
+app.set('view engine', 'hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+
+
+
+//setup static directory to use
+app.use(express.static(publicDirPath))
+
+
+
+
+//these are get routes
+
+app.get('',(req,res) => {
+  res.render('index')
+})
+
+app.get('*',(req,res) => {
+  res.render('404')
+})
+
+app.listen(3000, () => {
+  console.log('server is up on port 3000')
+})
